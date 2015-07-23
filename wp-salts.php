4e7a79ac0268404e62fbd91f1c92078893c68b76
Gaspar Nemes <gaspar@mooveagency.com>
2015-07-23 09:46:20 +0100

    Test for vagrant

---
+++ wp-salts.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         'i|sx-|45w1Ucuj8Zrqy+iXff-@V3NLOE&^[de#W#>g,Xte(Eg7*Q`yQV4},DiMjf');
+define('SECURE_AUTH_KEY',  '?pjq/pk*dy&g>`_VN]qQ,AJ(7pXu!|Xz[K 3-a;;~%,MmbPYQ7aS;!_b:0-+jb;Z');
+define('LOGGED_IN_KEY',    '?d:7quxp7,0o8H<lMC0e$ZvRPx/ |P@nTqp{V-oKUlY6y.}?XPg-o^mjd-&O3DO5');
+define('NONCE_KEY',        '&chKoX;`A43F8TzHqG~6hZ>@T3p Yr~}n%K&P+}8zPIi%5&=Ky]PWs}3zC mFl5M');
+define('AUTH_SALT',        'yK(7$:W[65t^&&}PIXGbYeF&QM<Ca[F]feH@.b].K=/[oEV2W!0qUm54|I]c>TJQ');
+define('SECURE_AUTH_SALT', '&8s2&TJ-i3?,jKfWd@)Sj9_mP`xpJmWVDVsKOHtt|)eTY+@gx;bE|kWKoqAu*HJJ');
+define('LOGGED_IN_SALT',   'qxxdY>Qr#]H0O)Wx<.6w+- &4e5s/<JA^0$@4s8qN-jLZS$,k?ja3Ekhrvg~:@n|');
+define('NONCE_SALT',       'o$]C}[dMXiq)Kb&M<`--gi4-P63w4m7mU=Gu._RW97KOTAszL-Mg3%<v:?Dw%m|Z');
